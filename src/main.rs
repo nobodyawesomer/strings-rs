663b87a070c3bc6ebf105555ed011ecd0227ecf4
Nobody <nobodyawesomer@proton.me>
2022-11-14 18:07:06 -0800

    Basic Functionality!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::fs;
+
 use clap::Parser;
+use eyre::Result;
+use regex::bytes::Regex;
 
 #[derive(Parser)]
 struct Cli {
@@ -11,9 +15,27 @@ struct Cli {
 	files: Vec<String>,
 }
 
-fn main() {
+fn main() -> Result<()> {
 	let cli = Cli::parse();
 	let min_len = cli.bytes.unwrap_or(4);
 
 	eprintln!("Filtering for a length of {min_len}");
+	let re = {
+		// ASCII-compatible mode
+		// Capture sequence of printable characters, ending in Null
+		// Substitute in with format!-escaped minimum length
+		let re = format!(r"(?ms)([\s\x20-\x7e]{{{min_len},}})\x00");
+		Regex::new(&re).unwrap()
+	};
+
+	for file in cli.files {
+		let bytes = fs::read(file)?;
+		for cap in re.captures_iter(&bytes) {
+			let s = String::from_utf8_lossy(&cap[1]); // guaranteed 1 capture on match
+										  // println!("++++++++++++++++++++++++\n{s}\n------------------------");
+			println!("{s}");
+		}
+	}
+
+	Ok(())
 }
